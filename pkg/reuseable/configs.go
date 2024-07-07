cf726e4592b4a44a04cda4e838978a577cfdfa1f
Ayotola Ajisola <49092282+Ajinx1@users.noreply.github.com>
2024-07-07 14:02:02 +0100

    removed dcoker for now

--- pkg/reuseable/configs.go
+++ pkg/reuseable/configs.go
@@ -1,19 +1,16 @@
 package reuseable
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 // ///////////////////////////////////////////
 // Load env file and get value
 // ///////////////////////////////////////////
 func GetEnvVar(theValue string) string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	return os.Getenv(theValue)
 }
